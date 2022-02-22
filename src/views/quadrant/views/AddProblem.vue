186dcf42a76315c2f1acb85b92497c94f6d24065
Ayush Goyal <ayush.goyal@research.iiit.ac.in>
2022-02-23 01:41:30 +0530

    Fix buggy integer input.

--- src/views/quadrant/views/AddProblem.vue
+++ src/views/quadrant/views/AddProblem.vue
@@ -66,7 +66,7 @@
               <FormulateInput name="is_correct" type="checkbox" label="Is Correct?" />
             </FormulateInput>
             <FormulateInput
-              v-if="problem.problem_type=='I'"
+              v-if="problem.problem_type==='I'"
               name="correct_integer"
               type="number"
               label="Correct Integer"
