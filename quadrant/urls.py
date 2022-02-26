d2bcd8ba9d72971a4d2775f0830910f251796c7c
Anvay Karmore <anvay.karmore@students.iiit.ac.in>
2022-02-26 15:35:27 +0530

    Added elo app

---
+++ quadrant/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+urlpatterns=[
+    # '/quadregister' ## registers for quadrant with mobile number and credentials
+    # '/contests/<uuid:contest_uuid>/quadpply ## apply as writers
+    # '/contests/' ## get list of all the open contests
+    # '/contests/<uuid:contest_uuid>/problems/' ## get all the problems and post to add a problem(only if you are accepted as a writer)
+    # '/contests/<uuid:contest_uuid>/problems/<uuid:problem_uuid>/' ##get, patch, delete the problems
+    # '/self/contests/' ## get your own contests
+
+]
