f458b4f695df03a2e58f85f05c6ff28b872cbdf6
Ayush Goyal <ayush.goyal@research.iiit.ac.in>
2022-03-25 14:03:17 +0530

    modified Username validation

--- appauth/validators.py
+++ appauth/validators.py
@@ -10,7 +10,7 @@ def check_username_taken(username):
 
 
 def check_username_valid(username):
-    if not re.match('^[a-z][0-9a-z]*$', username):
+    if not re.match('^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$', username):
         raise ValidationError("Username not valid")
 
 
