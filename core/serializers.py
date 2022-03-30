0940e5bbd981616bd3cb8b74c869a220d1f20b16
Anvay Karmore <anvay.karmore@students.iiit.ac.in>
2022-03-30 12:19:54 +0530

    Modified /contest to add  contestprocess'

--- core/serializers.py
+++ core/serializers.py
@@ -14,67 +14,6 @@ class ScoreSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ContestPreviewSerializer(serializers.ModelSerializer):
-    writers = serializers.SerializerMethodField()
-    is_applied = serializers.SerializerMethodField()
-    is_attempted = serializers.SerializerMethodField()
-    status = serializers.SerializerMethodField()
-    question_count = serializers.SerializerMethodField()
-    # authors = serializers.SerializerMethodField()
-    attempt = serializers.SerializerMethodField()
-    class Meta:
-        model = Contest
-        fields = [
-            'uuid',
-            'name',
-            'contest_status',
-            'date_created',
-            'target_date',
-            'end_date',
-            'duration',
-            'contest_type',
-            'contest_difficulty',
-            'writers',
-            'description',
-            'contest_chips',
-            'is_applied',
-            'is_attempted',
-            'status',
-            'question_count',
-            'attempt',
-            # 'authors'
-        ]
-
-    def get_attempt(self,contest):
-        user = self.context['request'].user
-        if Contestprocess.objects.filter(user=user,contest=contest).exists():
-            contest_process = Contestprocess.objects.get(user=user,contest=contest)
-            if contest_process.attempt:
-                return True
-        return False
-    def get_writers(self, contest):
-        users = User.objects.filter(
-            quadapplications__contest=contest, quadapplications__is_accepted=True)
-        return UserPreviewSerializer(users, many=True).data
-
-    def get_is_applied(self, contest):
-        user = self.context['request'].user
-        if Contestprocess.objects.filter(user=user, contest=contest).exists():
-            return True
-        return False
-
-    def get_is_attempted(self, contest):
-        user = self.context['request'].user
-        if Contestprocess.objects.filter(user=user, attempt=True).exists():
-            return True
-        return False
-
-    def get_question_count(self, contest):
-        return contest.problems.count()
-
-    def get_status(self, contest):
-        return contest.status()
-
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
@@ -185,5 +124,76 @@ class StandingsModelSerializer(serializers.ModelSerializer):
             'rating',
             'rating_change',
             'score',
-            'rated_date'
+            'rated_date',
+            'status',
         ]
+class ContestPreviewSerializer(serializers.ModelSerializer):
+    writers = serializers.SerializerMethodField()
+    is_applied = serializers.SerializerMethodField()
+    is_attempted = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    question_count = serializers.SerializerMethodField()
+    # authors = serializers.SerializerMethodField()
+    attempt = serializers.SerializerMethodField()
+    contest_process = serializers.SerializerMethodField()
+    class Meta:
+        model = Contest
+        fields = [
+            'uuid',
+            'name',
+            'contest_status',
+            'date_created',
+            'target_date',
+            'end_date',
+            'duration',
+            'contest_type',
+            'contest_difficulty',
+            'writers',
+            'description',
+            'contest_chips',
+            'is_applied',
+            'is_attempted',
+            'status',
+            'question_count',
+            'attempt',
+            'contest_process'
+            
+            # 'authors'
+        ]
+    def get_contest_process(self,contest):
+        user = self.context['request'].user
+
+        if Contestprocess.objects.filter(user=user,contest=contest).exists():
+            contest_process = Contestprocess.objects.get(user=user,contest=contest)
+            return StandingsModelSerializer(contest_process).data
+        return None
+
+    def get_attempt(self,contest):
+        user = self.context['request'].user
+        if Contestprocess.objects.filter(user=user,contest=contest).exists():
+            contest_process = Contestprocess.objects.get(user=user,contest=contest)
+            if contest_process.attempt:
+                return True
+        return False
+    def get_writers(self, contest):
+        users = User.objects.filter(
+            quadapplications__contest=contest, quadapplications__is_accepted=True)
+        return UserPreviewSerializer(users, many=True).data
+
+    def get_is_applied(self, contest):
+        user = self.context['request'].user
+        if Contestprocess.objects.filter(user=user, contest=contest).exists():
+            return True
+        return False
+
+    def get_is_attempted(self, contest):
+        user = self.context['request'].user
+        if Contestprocess.objects.filter(user=user, attempt=True).exists():
+            return True
+        return False
+
+    def get_question_count(self, contest):
+        return contest.problems.count()
+
+    def get_status(self, contest):
+        return contest.status()
